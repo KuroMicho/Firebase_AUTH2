59cf87fe255a9fd9c290372d459db594a7181ea3
KuroMicho <kevin0552009@hotmail.com>
2019-05-20 15:05:53 -0500

    nuevo

---
+++ pages/contacto.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-fluid" style="margin-top:20px">
+    <form>
+      <div class="row">
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+          <a href="#" target="_blank" rel="nofollow">
+            <img
+              height="80"
+              width="200"
+              class="lazy"
+              src="https://firebasestorage.googleapis.com/v0/b/nuxt-7dafb.appspot.com/o/twitter.png?alt=media&token=7575bb85-faaa-4451-9448-44cdf0edd2c6"
+            >
+          </a>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+          <a href="#" target="_blank" rel="nofollow">
+            <img
+              height="80"
+              width="200"
+              class="lazy"
+              src="https://i.pinimg.com/originals/d0/64/54/d06454c272ea4564e674a428af7d9456.png"
+            >
+          </a>
+        </div>
+
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+          <a href="#" target="_blank" rel="nofollow">
+            <img
+              height="80"
+              width="200"
+              class="lazy"
+              src="https://firebasestorage.googleapis.com/v0/b/nuxt-7dafb.appspot.com/o/google%2B.png?alt=media&token=3c211c63-68e6-4866-90b9-43866dc06821"
+            >
+          </a>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+          <a href="#" target="_blank" rel="nofollow">
+            <img
+              height="80"
+              width="200"
+              class="lazy"
+              src="        https://pngimage.net/wp-content/uploads/2018/06/siguenos-en-instagram-png-2.png
+"
+            >
+          </a>
+        </div>
+      </div>
+    </form>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe harum laboriosam iusto assumenda excepturi fugiat, rem repellendus ut voluptas ducimus veniam ullam iure cumque at placeat vero reiciendis voluptatem! Dolores tempora sint possimus? Tempora vel beatae eligendi nostrum reiciendis delectus, vitae, aliquam ipsum, commodi sint culpa libero suscipit deleniti blanditiis eum architecto ratione cumque sunt quidem est accusantium. Hic repudiandae obcaecati sequi? Sit suscipit laborum, quis officia est, molestias voluptatum tempora minima perferendis et nulla pariatur earum reprehenderit, esse in. Error molestias aspernatur labore impedit consequatur, magnam vitae distinctio, sapiente nesciunt repudiandae doloribus maxime, quis porro recusandae odio? Ipsam, consectetur?</p>
+  </div>
+</template>
